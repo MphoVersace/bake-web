900616fdf99ade7f2cdb870e951dd0ef0a87c5d8
MphoVersace <mtlomelompho@gmail.com>
2024-06-14 15:25:29 +0200

    image added,renamed and move into assets

--- bake-web/src/MenuPage.jsx
+++ bake-web/src/MenuPage.jsx
@@ -5,7 +5,7 @@ import cake3 from './assets/cake3.jpeg';
 import cake4 from './assets/cake4.jpeg';
 import cake5 from './assets/cake5.jpeg';
 import cake6 from './assets/cake6.jpeg';
-import burger7 from './assets/image7.jpeg';
+import cake7 from './assets/cake7.jpeg';
 import burger8 from './assets/image8.jpeg';
 import burger9 from './assets/image9.jpeg';
 import burger10 from './assets/image10.jpeg';
@@ -19,7 +19,7 @@ const MenuPage = () => {
     { name: 'Lemon Zest Cupcakes', img: cake4, description: 'A refreshing cupcake with a bright, zesty lemon flavor, topped with a tangy lemon buttercream frosting and a candied lemon peel garnish. Perfect for a burst of citrusy goodness..' },
     { name: 'Salted Caramel Cupcakes', img: cake5, description: 'A moist vanilla cupcake filled with a gooey salted caramel center, topped with a swirl of caramel buttercream and a sprinkle of sea salt. The perfect blend of sweet and salty..' },
     { name: 'Strawberry Shortcake Cupcakes', img: cake6, description: 'Light and airy vanilla cupcake infused with fresh strawberries, topped with a fluffy whipped cream frosting and a juicy strawberry slice. A delightful twist on a classic dessert..' },
-    { name: 'BBQ Burger', img: burger7, description: 'A sweet and sour BBQ burger.' },
+    { name: 'Peanut Butter Cupcakes', img: cake7, description: 'A rich peanut butter-flavored cupcake topped with creamy peanut butter frosting and a drizzle of chocolate ganache. Perfect for peanut butter lovers.' },
     { name: 'Peri Peri Original', img: burger8, description: 'The iconic Black Mamba Peri-Peri sauce on our Original Chicken Sandwich.' },
     { name: 'Texas BBQ King', img: burger9, description: 'The Texas BBQ KING features two flame-grilled beef patties, our signature crispy onion rings, tangy BBQ sauce, melted.' }, // Added comma
     { name: ' Chicken Cheese', img: burger10, description:'Classic ingredients flavoured just right. You can’t go wrong with our Cheeseburger, a signature flame-grilled beef patty.' } // Added comma

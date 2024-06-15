1beeee50fc1cef390d34b8d943bd28807db4eb61
MphoVersace <mtlomelompho@gmail.com>
2024-06-15 14:29:22 +0200

    home update

--- bake-web/src/HomePage.jsx
+++ bake-web/src/HomePage.jsx
@@ -14,14 +14,14 @@ import './styles.css';
 
 const HomePage = () => {
   const images = [
-    { src: home1},
-    { src: home2},
-    { src: home3},
-    { src: home4},
-    { src: home5},
-    { src: home6},
-    { src: home7},
-    { src: home8},
+    { src: home1 },
+    { src: home2 },
+    { src: home3 },
+    { src: home4 },
+    { src: home5 },
+    { src: home6 },
+    { src: home7 },
+    { src: home8 },
   ];
 
   return (
@@ -37,10 +37,23 @@ const HomePage = () => {
       >
         {images.map((image, index) => (
           <div key={index} className="carousel-image-container">
-            <img src={image.src} alt={image.alt} className="carousel-image" />
+            <img src={image.src} alt={`Slide ${index + 1}`} className="carousel-image" />
+            <div className="carousel-caption">
+              <p className="carousel-caption-text">
+                <strong>The Home Of Cupcakes</strong>
+                <br />
+                HOME OF CUPCAKES is a family-owned artisanal patisserie and coffee shop with a proud American heritage dating back to 1889. Known for our personal touch, impeccable service levels and warm, sophisticated atmosphere,  <em>HOME OF CUPCAKES</em> offers a vast American style menu to suit any occasion, from diet cupcakes right through to sweet and every coffee and teatime in between.
+                <br />
+                We are all about people, community and great food, delivered to you with unapologetic American flair. Our gourmet meals and freshly baked cupcakes, pastries and confectionary, are made daily from scratch with carefully selected quality ingredients. Our customers pop in for a bite, business meeting or catch up with friends with many loyalists visiting us on a regularly basis.
+              </p>
+            </div>
           </div>
         ))}
       </Carousel>
+      <hr className="divider-line" />
+      <h1>Our Commitment</h1>
+      <hr className="divider-line" />
+      <p>For five generations,<em> Home Of Cupcakes</em> has played a crucial role in the community, recognising the importance of protecting the environment and bringing quality products to people all over the world.</p>
     </div>
   );
 };
